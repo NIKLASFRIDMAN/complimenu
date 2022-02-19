2219ec7c12dea8a61838039489bacfe6bc66be16
peder frang <peder.frang@gmail.com>
2022-02-19 16:21:28 +0100

    save_and_create_a_new_order_on_homepage_load

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,12 @@
 class ItemsController < ApplicationController
+  skip_before_action :authenticate_user!
+  before_action :find_order
+
+
+  def index
+  end
+
+  def find_order
+    @order = Order.find(session[:order_id])
+  end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,8 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    order = Order.new
+    order.save!
+    session[:order_id] = order.id
   end
 end
