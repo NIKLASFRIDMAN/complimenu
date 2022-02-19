5bc5894c82ee832482c242df4dac63ed26dafd3e
Florencia Tarocco <florenciatarocco@gmail.com>
2022-02-19 15:35:17 +0100

    create route, index method and index view

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,10 @@
 class ItemsController < ApplicationController
+  def index
+    @items = Item.all
+    @categories = []
+    @items.each do |item|
+      @categories << item.category
+    end
+    return @categories.uniq!
+  end
 end
