83b5831d1f472b5627366e78a9911824f6413403
Florencia Tarocco <florenciatarocco@gmail.com>
2022-02-22 21:34:46 +0100

    add item to order

--- app/controllers/item_orders_controller.rb
+++ app/controllers/item_orders_controller.rb
@@ -2,6 +2,14 @@ class ItemOrdersController < ApplicationController
   skip_before_action :authenticate_user!
   before_action :find_order
 
+  def create
+    item_order = ItemOrder.new(quantity: 1)
+    item_order.item_id = params[:item_id]
+    item_order.order_id = @order.id
+    item_order.save!
+    redirect_to items_path
+  end
+
   def find_order
     @order = Order.find(session[:order_id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :items, only: [:index]
+  resources :items, only: [:index] do
+    resources :item_orders, only: [:create]
+  end
+  resources :item_orders, only: [:update, :destroy]
+
   # TODO namespace for routing
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :items, only: [:index]
 end
