4aa5b1158f08f063a147136b16bb15d1726f28db
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-03-01 19:21:55 +0100

    refactor

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,10 +3,11 @@ class OrdersController < ApplicationController
 
   def show
     @order = Order.find(session[:order_id])
-    @all_items_total_price = []
+    @all_items_total_price = 0
     @order.item_orders.each do |item_order|
-      @all_items_total_price << (item_order.item.price * item_order.quantity)
+      @all_items_total_price += (item_order.item.price * item_order.quantity)
     end
+    @all_items_total_price = @all_items_total_price.round(2)
   end
 
   def checkout
