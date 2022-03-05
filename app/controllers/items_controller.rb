93fe1ae7581fa0e4c93a6b4efea3f7771a44431a
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-05 12:12:36 +0000

    Refactor queries to retrive items and itemOrders

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,8 +3,11 @@ class ItemsController < ApplicationController
 
   def index
     redirect_to "#{items_path}##{params[:format].downcase}" if params[:format]
-    @catitems = ItemOrder.includes(:item)
-                         .distinct
-                         .group_by { |item_order| item_order.item.category }
+    @category_items = Item.includes(:item_orders)
+                          .distinct
+                          .group_by { |item| item.category }
+    @item_orders = ItemOrder.joins(:item)
+                            .where(order_id: 3)
+                            .group_by { |item_order| item_order.item.id }
   end
 end
