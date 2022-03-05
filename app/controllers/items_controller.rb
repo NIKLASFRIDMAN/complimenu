328b8294dbe54cd13ad6b0a61eb3ab5a4047ed70
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-05 11:21:39 +0000

    refactor controller code a bit more

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,18 @@ class ItemsController < ApplicationController
     redirect_to "#{items_path}##{params[:format].downcase}" if params[:format]
     @catitems = {}
     items = Item.all
-    Item.distinct.pluck(:category).each do |category|
-      items_cat = items.select { |i| i.category = category }
-      itemorders = ItemOrder.all
-      @catitems[category.to_sym] = []
-      items_cat.each do |item|
-        @catitems[category.to_sym] << { item: item,
-                                        item_order: itemorders.select { |io| io.item_id == item.id && io.order_id == session[:order_id] }[0] }
-      end
+    categories = Item.distinct.pluck(:category)
+    categories.each do |category|
+      
+      item_order = ItemOrder.joins(:item).where(item: {category: category})
+      @catitems[category.to_sym] = item_order
+      # items_cat = items.select { |i| i.category = category }
+      # itemorders = ItemOrder.all
+      # @catitems[category.to_sym] = []
+      # items_cat.each do |item|
+      #   @catitems[category.to_sym] << { item: item,
+      #                                   item_order: itemorders.select { |io| io.item_id == item.id && io.order_id == session[:order_id] }[0] }
+      # end
     end
   end
 end
