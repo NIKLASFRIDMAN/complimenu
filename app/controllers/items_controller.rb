e4fee067a1f196bf4c3f5e4f372f4d3fd9deb039
Florencia Tarocco <florenciatarocco@gmail.com>
2022-03-05 15:13:56 +0100

    add quantity to basket button

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,14 +5,25 @@ class ItemsController < ApplicationController
     redirect_to "#{items_path}##{params[:format].downcase}" if params[:format]
     @catitems = {}
     items = Item.all
+    itemorders = ItemOrder.all
     items.map(&:category).uniq.each do |category|
       items_cat = items.select { |i| i.category = category }
-      itemorders = ItemOrder.all
       @catitems[category.to_sym] = []
       items_cat.each do |item|
         @catitems[category.to_sym] << { item: item,
                                         item_order: itemorders.select { |io| io.item_id == item.id && io.order_id == session[:order_id] }[0] }
       end
     end
+    @total_items = total_items(itemorders)
   end
+end
+
+private
+
+def total_items(itemorders)
+  total_items = 0
+  itemorders.each do |itemorder|
+    total_items += itemorder.quantity
+  end
+  return total_items
 end
