b2f10555d4b2574f4f0faf44344641052652f276
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-05 13:55:28 +0000

    Fix controller filters as discussed in the call

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,11 +3,11 @@ class ItemsController < ApplicationController
 
   def index
     redirect_to "#{items_path}##{params[:format].downcase}" if params[:format]
-    @category_items = Item.includes(:item_orders)
-                          .distinct
+    @category_items = Item.joins(:item_orders)
                           .group_by { |item| item.category }
+
     @item_orders = ItemOrder.joins(:item)
-                            .where(order_id: 3)
+                            .where(order_id: session[:order_id])
                             .group_by { |item_order| item_order.item.id }
   end
 end
