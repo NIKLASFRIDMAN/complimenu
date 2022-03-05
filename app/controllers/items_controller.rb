7c5d812015b90dd3220c76236f0ec7df10bf7cba
Andre Ferrer <andrerferrer@hotmail.com>
2022-03-05 11:39:19 +0000

    refactor it all into one query

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,22 +3,8 @@ class ItemsController < ApplicationController
 
   def index
     redirect_to "#{items_path}##{params[:format].downcase}" if params[:format]
-    @catitems = {}
-    categories = Item.distinct.pluck(:category)
-    categories.each do |category|
-      
-      item_order = ItemOrder.includes(:item)
-                            .joins(:item)
-                            .where(item: {category: category})
-
-      @catitems[category.to_sym] = item_order
-      # items_cat = items.select { |i| i.category = category }
-      # itemorders = ItemOrder.all
-      # @catitems[category.to_sym] = []
-      # items_cat.each do |item|
-      #   @catitems[category.to_sym] << { item: item,
-      #                                   item_order: itemorders.select { |io| io.item_id == item.id && io.order_id == session[:order_id] }[0] }
-      # end
-    end
+    @catitems = ItemOrder.includes(:item)
+                         .distinct
+                         .group_by { |item_order| item_order.item.category }
   end
 end
