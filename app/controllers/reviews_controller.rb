5781aa2e86c5cf83e86f7d4a32d34295216f1bcf
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-03-05 16:06:33 +0100

    review view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,17 +1,20 @@
 class ReviewsController < ApplicationController
+    skip_before_action :authenticate_user!, only: [ :new, :create ]
+    before_action :find_order, only: [:new, :create]
+
+
   def new
     @review = Review.new
   end
 
   def create
     @review = Review.new(review_params)
-    order = Order.find(session[:order_id])
     item_order = ItemOrder.find_by(order_id: session[:order_id])
     item = item_order.item
-    @review.order = order
+    @review.order = @order
     @review.item = item
     if @review.save
-      redirect_to new_review_path
+      redirect_to root_path
     else
       flash[:alert] = "Something went wrong."
       render :new
@@ -19,6 +22,10 @@ class ReviewsController < ApplicationController
   end
 
   private
+
+  def find_order
+    @order = Order.find(session[:order_id])
+  end
 
   def review_params
     params.require(:review).permit(:upvote, :downvote)
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   resources :items, only: [:index] do
-    resources :item_orders, only: [:create] do
-      resources :reviews, only: [:new, :create]
-    end
+    resources :item_orders, only: [:create]
   end
 
   resources :item_orders, only: [:update, :destroy]
@@ -13,6 +11,11 @@ Rails.application.routes.draw do
   get '/checkout', to: 'orders#checkout'
   get '/qrcode', to: 'pages#qrcode'
   get '/waiter', to: 'orders#waiter'
+
+  resources :reviews, only: [:new, :create]
+
+  get '/review', to: 'reviews#new', as: "review"
+  get '/review', to: 'reviews#create'
 
   # TODO namespace for routing
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
