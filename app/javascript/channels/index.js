35bd2ffb6561e7d90a96ed82d52daf402124cc26
NIKLAS FRIDMAN <fridman.niklas@gmail.com>
2022-03-17 18:38:27 +0100

    toggle

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,12 @@
 // Load all the channels within this directory and all subdirectories.
 // Channel files must be named *_channel.js.
 
+
+const button = document.querySelector("h1")
+  button.addEventListener("click", () => {
+    button.classList.toggle("plus-minus-button-toggle")
+})
+
+
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
